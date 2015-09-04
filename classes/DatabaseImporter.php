85aaf86e6748843af1a98d16eb72ab79a837801b
Dennis Patzer <d.patzer@heimrich-hannot.de>
2015-09-04 14:35:45 +0200

    added insertId

--- classes/DatabaseImporter.php
+++ classes/DatabaseImporter.php
@@ -48,7 +48,66 @@ class DatabaseImporter extends Importer
 		}
 
 		$strQuery = "INSERT INTO $t (" . implode(',', array_keys($arrItem)) . ") VALUES(" . implode(',', array_map(function($val) { return "'" . $val . "'"; }, array_values($arrItem))) . ")";
-		$objDatabase->execute($strQuery);
+<?php
+
+namespace HeimrichHannot\EntityImport;
+
+class DatabaseImporter extends Importer
+{
+
+	/**
+	 * run the importer
+	 *
+	 * @return bool
+	 */
+	public function run()
+	{
+		$this->collectItems();
+
+		if ($this->objItems === null) {
+			return false;
+		}
+
+		// special handling for creating products via DcaManager
+		while ($this->objItems->next()) {
+			$arrItem = $this->createObjectFromMapping($this->objItems);
+			$this->createImportMessage($arrItem);
+		}
+
+		return true;
+	}
+
+	protected function createObjectFromMapping($objSourceItem)
+	{
+		$objDatabase = \Database::getInstance();
+		\Controller::loadDataContainer($this->dbTargetTable);
+
+		$dca = $GLOBALS['TL_DCA'][$this->dbTargetTable];
+		$t = $this->dbTargetTable;
+
+		$arrItem = array();
+
+		foreach ($this->arrMapping as $key => $col) {
+			$value = $this->setValueByType($objSourceItem->{$key}, $dca['fields'][$key]);
+			$this->setObjectValueFromMapping($arrItem, $value, $key);
+
+			if ($value === null) {
+				unset($arrItem[$key]);
+				continue;
+			}
+		}
+
+		$strQuery = "INSERT INTO $t (" . implode(',', array_keys($arrItem)) . ") VALUES(" . implode(',', array_map(function($val) { return "'" . $val . "'"; }, array_values($arrItem))) . ")";
+
+		$arrItem['id'] = $objDatabase->execute($strQuery)->insertId;
+
+		// do after item has been created,
+		$this->runAfterSaving($arrItem, $objSourceItem);
+
+		return $arrItem;
+	}
+
+}
 
 		// do after item has been created,
 		$this->runAfterSaving($arrItem, $objSourceItem);
