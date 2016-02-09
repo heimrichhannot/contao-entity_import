e5e18fda6dbbafeaa60c0252762e1e14a636112d
Dennis Patzer <d.patzer@heimrich-hannot.de>
2016-02-09 14:22:48 +0100

    reverted DatabaseImporter

--- classes/DatabaseImporter.php
+++ classes/DatabaseImporter.php
@@ -20,77 +20,46 @@ class DatabaseImporter extends Importer
 			return false;
 		}
 
-		$strClass = \Model::getClassFromTable($this->dbTargetTable);
-
-		if (!class_exists($strClass)) {
-			return false;
-		}
-
 		// special handling for creating products via DcaManager
 		while ($this->objItems->next()) {
-			$arrItem = $this->createObjectFromMapping($this->objItems, $strClass);
+			$arrItem = $this->createObjectFromMapping($this->objItems);
 			$this->createImportMessage($arrItem);
 		}
 
 		return true;
 	}
 
-	protected function createObjectFromMapping($objSourceItem, $strClass)
+	protected function createObjectFromMapping($objSourceItem)
 	{
-		$objItem = new $strClass();
-
+		$objDatabase = \Database::getInstance();
 		\Controller::loadDataContainer($this->dbTargetTable);
 
 		$dca = $GLOBALS['TL_DCA'][$this->dbTargetTable];
+		$t = $this->dbTargetTable;
 
-		$dca = $GLOBALS['TL_DCA'][$this->dbTargetTable];
+		$arrItem = array();
 
-		// update existing items
-		if (in_array($objItem->getPk(), array_keys($this->arrMapping)))
-		{
-			$objUpdateItem = $strClass::findByPk($objSourceItem->{$objItem->getPk()});
-
-			if($objUpdateItem !== null)
-			{
-				$objItem = $objUpdateItem;
-			}
-		}
-
-		foreach ($this->arrMapping as $key => $col)
-		{
-			$value                = $this->setValueByType($objSourceItem->{$key}, $dca['fields'][$key], $objItem, $objSourceItem);
-			$this->setObjectValueFromMapping($objItem, $value, $key);
+		foreach ($this->arrMapping as $key => $col) {
+			$value = $this->setValueByType($objSourceItem->{$key}, $dca['fields'][$key]);
+			$this->setObjectValueFromMapping($arrItem, $value, $key);
 
 			if ($value === null) {
+				unset($arrItem[$key]);
 				continue;
 			}
-
-			// do not save in dry run
-			if ($this->dryRun) {
-				continue;
-			}
-
-			$objItem->save();
 		}
 
-		// do after item has been created, no in dry mode
 		if (!$this->dryRun)
 		{
-			$this->runAfterSaving($objItem, $objSourceItem);
+			$strQuery = "INSERT INTO $t (" . implode(',', array_keys($arrItem)) . ") VALUES(" . implode(',', array_map(function($val) { return "'" . $val . "'"; }, array_values($arrItem))) . ")";
 
+			$arrItem['id'] = $objDatabase->execute($strQuery)->insertId;
 
-			// HOOK: run after saving callback
-			if (isset($GLOBALS['TL_HOOKS']['entityImportRunAfterSaving']) && is_array($GLOBALS['TL_HOOKS']['entityImportRunAfterSaving']))
-			{
-				foreach ($GLOBALS['TL_HOOKS']['entityImportRunAfterSaving'] as $callback)
-				{
-					$this->import($callback[0]);
-					$objItem = $this->$callback[0]->$callback[1]($objItem, $objSourceItem, $this);
-				}
-			}
+			// do after item has been created,
+			$this->runAfterSaving($arrItem, $objSourceItem);
 		}
 
-		return $objItem;
+		return $arrItem;
 	}
 
 }
